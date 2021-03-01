59c216dfa703dbdd9932e2db9e26a2fa9d57a40b
sora.okabe <d.sora1116@gmail.com>
2021-03-01 21:26:16 +0900

    aaa

---
+++ app/src/main/java/sora/janken/MainActivity.kt
@@ -0,0 +1,27 @@
+package sora.janken
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import sora.janken.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gu.setOnClickListener { onJankenButtonTapped(it) }
+        binding.choki.setOnClickListener { onJankenButtonTapped(it) }
+        binding.pa.setOnClickListener { onJankenButtonTapped(it) }
+    }
+
+    fun onJankenButtonTapped(view: View?) {
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("MY_HAND",view?.id)
+        startActivity(intent)
+    }
+
+}
